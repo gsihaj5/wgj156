a901170fb2f0ced9f4910fcf34b56a6c9fb57c83
gsihaj5 <gsihaj5@gmail.com>
2020-07-07 08:31:24 +0700

    init

---
+++ Assets/Player/CrossHair/CrossHairHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHairHandler : MonoBehaviour
+{
+    private Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        Vector3 velocity = this.gameObject.transform.parent.GetComponent<Rigidbody2D>().velocity;
+        
+        //if(angle % 90 == 0 && (movement.x != 0 || movement.y != 0))
+        if(velocity.x != 0 || velocity.y != 0)
+            transform.eulerAngles = new Vector3(0,0,angle);
+    }
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed;
+    private Vector2 movement;
+    
+    public Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    private void getInput(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void move(){
+        rigidBody.velocity = new Vector2(movement.x * speed, movement.y * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+        move();
+    }
+}
